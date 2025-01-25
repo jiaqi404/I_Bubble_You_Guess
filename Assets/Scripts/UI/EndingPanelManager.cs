0d0cefc3edd77a2e32786dde4604f76dfbfa5e08
Zhang Jiaqi <marycheung021213@gmail.com>
2025-01-25 18:10:22 +0800

    PC Pre-final version

--- Assets/Scripts/UI/EndingPanelManager.cs
+++ Assets/Scripts/UI/EndingPanelManager.cs
@@ -1,14 +1,24 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndingPanelManager : MonoBehaviour
 {
     public SettingsManager settingsManager;
     public TextMeshProUGUI propmtRevealText;
+    public Button restartBtn;
 
     void Awake()
     {
+        restartBtn.onClick.AddListener(restartGame);
         string prompt = settingsManager.prompt;
+        Debug.Log(prompt);
         propmtRevealText.text = "It's a " + prompt + "!";
+    }
+
+    void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
