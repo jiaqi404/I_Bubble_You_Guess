6d8c4bc268d45d171246de49db58c37a01a6a0d1
Zhang Jiaqi <marycheung021213@gmail.com>
2025-01-25 11:02:47 +0800

    Upload UI

--- Assets/Scripts/UI/PromptPanelManager.cs
+++ Assets/Scripts/UI/PromptPanelManager.cs
@@ -1,13 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class PromptPanelManager : MonoBehaviour
 {
     public float duration;
+    public TextMeshProUGUI counterText;
     float counter;
+    int counterRevert;
 
     void Update()
     {
         counter += Time.deltaTime;
+        counterRevert = (int)(duration - counter) + 1;
+        counterText.text = counterRevert.ToString();
         if (counter > duration)
         {
             gameObject.SetActive(false);
--- Assets/Scripts/UI/StartMenuManager.cs
+++ Assets/Scripts/UI/StartMenuManager.cs
@@ -9,7 +9,6 @@ public class StartMenuManager : MonoBehaviour
     public GameObject startMenu;
     public GameObject producerPanel;
     public GameObject promptPanel;
-    public GameObject gameManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,6 +31,7 @@ public class StartMenuManager : MonoBehaviour
     void GameExit()
     {
         Application.Quit();
+        Debug.Log("Game Exit!");
     }
 
     void Producers()
